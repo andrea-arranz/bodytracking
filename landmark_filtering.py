65c907f079dd2410f94103b13d71674f0d332aed
Antonio Aguirrre Ivorra <antonioai@antoniombp.local>
2021-12-16 01:09:41 +0100

    left arm works

--- landmark_filtering.py
+++ landmark_filtering.py
@@ -4,6 +4,7 @@ import numpy as np
 from time import time
 import mediapipe as mp
 import matplotlib.pyplot as plt
+from pygame import mixer
 # Initializing mediapipe pose class.
 mp_pose = mp.solutions.pose
 
@@ -13,17 +14,29 @@ def landmarkFiltering(landmarks, output_image, display=False):
     # right_elbow
     # right_shoulder
     # right_hip
-    label1 = calcAngle(landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value],
+    label1 = int(calcAngle(landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value],
                       landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value],
-                      landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value])
+                      landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value]))
 
-    label2 = calcAngle(landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value],
+    label2 = int(calcAngle(landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value],
                     landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value],
-                    landmarks[mp_pose.PoseLandmark.LEFT_HIP.value])
+                    landmarks[mp_pose.PoseLandmark.LEFT_HIP.value]))
 
-    label = "right: elbow, hip, shoulder" + str(int(label1)) + "\n" + "left: elbow, hip, shoulder" + str(int(label2))
-    cv2.putText(output_image, str(int(label1)), (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, (0,0,0), 2)
-    cv2.putText(output_image, str(int(label2)), (10, 60),cv2.FONT_HERSHEY_PLAIN, 2, (0,0,0), 2)
+    label1 = int(label1/5.6)
+    label2 = int(label2/4.5)
+
+    mixer.init()
+    label_name = str(label1).rjust(3, str(0))
+    if('000' == label_name):
+        label_name = '001'
+    filename = 'piano/{}.wav'.format(label_name)
+    mixer.music.load(filename)
+    mixer.music.play()
+
+
+
+    cv2.putText(output_image, str(label1), (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, (0,0,0), 2)
+    cv2.putText(output_image, str(label2), (10, 60),cv2.FONT_HERSHEY_PLAIN, 2, (0,0,0), 2)
 
     return output_image
 
